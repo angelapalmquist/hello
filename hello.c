318d60f07541dfe1de01a135fbfeb94c404ca3fb
angelapalmquist <apalmquist3@huskers.unl.edu>
2020-08-21 12:35:19 -0500

    Added major
    Added a line to print my major

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Angela Palmquist
+ *         apalmquist3@huskers.unl.edu
+ * Date: 2020/08/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Angela Palmquist\n");
+  printf("Computer Engineering\n");
+
+  return 0;
+}
